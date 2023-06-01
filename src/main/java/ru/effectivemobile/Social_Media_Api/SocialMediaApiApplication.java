6c3229469a80e2889e55da06acfb471c113e35e3
Dima <dmitriypospelov93@gmail.com>
2023-06-01 11:00:14 +0700

    Комит в котором: 1. Пом файл 2. Настройки конфигурации

---
+++ src/main/java/ru/effectivemobile/Social_Media_Api/SocialMediaApiApplication.java
@@ -0,0 +1,13 @@
+package ru.effectivemobile.Social_Media_Api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMediaApiApplication.class, args);
+	}
+
+}
